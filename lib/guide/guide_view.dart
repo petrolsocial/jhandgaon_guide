c385c2216c8060b6b428de0464dfa53819ef635a
tejas_sayankar <tejas.sayankar@petrol.social>
2022-06-06 16:35:16 +0530

    Added close button on top right corner

--- lib/guide/guide_view.dart
+++ lib/guide/guide_view.dart
@@ -56,114 +56,130 @@ class GuideView extends StatelessWidget {
     }
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                opacity: 0.7,
-                image: AssetImage('assets/images/paper-texture.png', package: 'jhandgaon_guide'),
-                fit: BoxFit.cover),
-          ),
-          child: Center(
-            child: SizedBox(
-              width: 800,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Center(
-                        child: Padding(
-                      padding: const EdgeInsets.all(25),
-                      child: Text(
-                        'Welcome To Jhandgaon',
-                        style: CustomTheme.theme1.textTheme.headline1,
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Text(
-                          'Content',
-                          style: CustomTheme.theme1.textTheme.headline2,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        children: [
-                          TextButton(
-                            onPressed: (() {
-                              Scrollable.ensureVisible(
-                                  introKey.currentContext!);
-                            }),
-                            child: const Text('Introduction'),
-                          ),
-                          TextButton(
-                            onPressed: (() {
-                              Scrollable.ensureVisible(
-                                  jhandgaonIntroKey.currentContext!);
-                            }),
-                            child: const Text('Jhandgaon Intro'),
-                          ),
-                          ...indexButtons,
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        'Intro',
-                        style: CustomTheme.theme1.textTheme.headline2,
-                        key: introKey,
-                      ),
-                    ),
-                    Column(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    opacity: 0.7,
+                    image: AssetImage('assets/images/paper-texture.png', package: 'jhandgaon_guide'),
+                    fit: BoxFit.cover),
+              ),
+              child: Center(
+                child: SizedBox(
+                  width: 800,
+                  child: SingleChildScrollView(
+                    child: Column(
                       children: [
+                        Center(
+                            child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: Text(
+                            'Welcome To Jhandgaon',
+                            style: CustomTheme.theme1.textTheme.headline1,
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Text(
+                              'Content',
+                              style: CustomTheme.theme1.textTheme.headline2,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                            children: [
+                              TextButton(
+                                onPressed: (() {
+                                  Scrollable.ensureVisible(
+                                      introKey.currentContext!);
+                                }),
+                                child: const Text('Introduction'),
+                              ),
+                              TextButton(
+                                onPressed: (() {
+                                  Scrollable.ensureVisible(
+                                      jhandgaonIntroKey.currentContext!);
+                                }),
+                                child: const Text('Jhandgaon Intro'),
+                              ),
+                              ...indexButtons,
+                            ],
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 100,
+                        ),
                         Padding(
                           padding: const EdgeInsets.all(20),
                           child: Text(
-                            Guide.INTRO_TEXT,
+                            'Intro',
+                            style: CustomTheme.theme1.textTheme.headline2,
+                            key: introKey,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Text(
+                                Guide.INTRO_TEXT,
+                                style: CustomTheme.theme1.textTheme.bodyText1,
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child:
+                              const Image(image: AssetImage('assets/images/jhandgaon_text_logo.png', package: 'jhandgaon_guide'),),
+                          key: jhandgaonIntroKey,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            Guide.JHANDGAON_RPG_INTRO,
                             style: CustomTheme.theme1.textTheme.bodyText1,
                             textAlign: TextAlign.justify,
                           ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image(image: AssetImage(
+                            Guide.JHANDGAON_IMAGE,
+                            package: 'jhandgaon_guide',),
+                            fit: BoxFit.cover,
+                          ),),
+                        
+                        ...campaignWidgets,
                       ],
                     ),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child:
-                          const Image(image: AssetImage('assets/images/jhandgaon_text_logo.png', package: 'jhandgaon_guide'),),
-                      key: jhandgaonIntroKey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        Guide.JHANDGAON_RPG_INTRO,
-                        style: CustomTheme.theme1.textTheme.bodyText1,
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Image(image: AssetImage(
-                        Guide.JHANDGAON_IMAGE,
-                        package: 'jhandgaon_guide',),
-                        fit: BoxFit.cover,
-                      ),),
-                    
-                    ...campaignWidgets,
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+            Align(
+              alignment: Alignment.topRight, 
+              child: IconButton(
+                icon: const Icon(
+                  Icons.close
+                ), 
+                iconSize: 40, 
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
